bd322a96e9ea32bff6fc3add8c59f352e17f93c5
Kolya Movchan <kmovchan261@gmail.com>
2023-09-06 17:53:14 +0300

    feat(cli): Create initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,5 +2,11 @@ import React from 'react'
 import { Navigation } from './routes/Navigation'
 
 export function App() {
-  return <Navigation />
+  return (
+    <>
+      <h1 className="text-center text-3xl mt-2 py-4">
+        Checkout Page for solar module
+      </h1>
+    </>
+  )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { HashRouter } from 'react-router-dom'
-import { store } from './app/store'
 import { App } from './App'
+import { store } from './app/store'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </Provider>
 )
